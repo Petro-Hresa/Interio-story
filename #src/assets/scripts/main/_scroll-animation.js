4ab66c44616f4862807e4f6bcb4474a9c07d734d
Hresa <gresapetro379@gmail.com>
2021-06-22 20:40:28 +0300

    add: button-scrol up

--- #src/assets/scripts/main/_scroll-animation.js
+++ #src/assets/scripts/main/_scroll-animation.js
@@ -3,9 +3,7 @@ const animItems = document.querySelectorAll('.scroll-animation');
 const counterNumbers = document.querySelectorAll('.counter__number');
 const speed = 200;
 
-let flag = false;
-
-
+// last-project start function
 setTimeout(function(){
 
     let projectsItem =  document.querySelectorAll('.grid-item');
@@ -14,19 +12,17 @@ setTimeout(function(){
 
         lastProjectsLink.forEach(item =>{
             item.classList.add("animation-active");
-    
         })
     }
 
     projectsItem.forEach(project =>{
         project.classList.add("animation-active");
-
     })
     
 },60)
 
 
-
+// scroll animation
 if(animItems.length > 0){
    
     window.addEventListener('scroll' , animationForScroll )
@@ -51,34 +47,16 @@ if(animItems.length > 0){
 
               animItemPoint = window.innerHeight - animItemsHeight - animStart;
 
-           
-
             }
 
             if((pageYOffset > animItemsOffset - animItemPoint ) && pageYOffset < (animItemsOffset + animItemsHeight ) ){
 
                 animItem.classList.add("animation-active");
 
-                if(animItem.classList.contains("counter__number") && animItem.classList.contains("animation-active") && flag == false ){
-                    // console.log(animItem)
-                    // counterStart()  
-                    flag = true
-                
-                }
-
-                // if(number.classList.contains("animation-active")){
-                   
-                // }
-
-             
-
             }else{
 
               
                 if(!animItem.classList.contains("animate-none")){
-
-       
-                   
                     animItem.classList.remove("animation-active");
                 }
                 
@@ -89,6 +67,7 @@ if(animItems.length > 0){
      
     }
 
+    // offset page 
     function offset(element){
         const rect = element.getBoundingClientRect();
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -104,58 +83,5 @@ if(animItems.length > 0){
 
 
 
-//  function counterStart(){
-    // counterNumbers.forEach(number => { 
-    //     console.log(number)
-    
-    //     const updateCount = () =>{
-    //         // console.log(number)
-    //         const numberTarget = +number.getAttribute('data-target');
-    //         const numberInnerText = +number.innerText;
-           
-    //         const increment =  Math.round(numberTarget / speed);
-               
-    //         if(numberInnerText < numberTarget ){
-    //             //   console.log(increment)
-    //             number.innerText = numberInnerText + increment;
-    //             setTimeout(updateCount, 1)
 
-    //         }else number.innerText = numberTarget;
-    //     }
-
-       
-
-    //     })
-        // updateCount(); 
-    // }              
-        
-    // counterStart() 
-    
-        
-
-    		// jQuery Count To
-		// if ( plugins.counter.length ) {
-		// 	for ( var i = 0; i < plugins.counter.length; i++ ) {
-		// 		var
-		// 			counter = $(plugins.counter[i]),
-		// 			initCount = function () {
-		// 				var counter = $(this);
-		// 				if ( !counter.hasClass( "animated-first" ) && isScrolledIntoView( counter ) ) {
-		// 					counter.countTo({
-		// 						refreshInterval: 40,
-		// 						speed: counter.attr("data-speed") || 1000,
-		// 						from: 0,
-		// 						to: parseInt( counter.text(), 10 )
-		// 					});
-		// 					counter.addClass('animated-first');
-		// 				}
-		// 			};
-
-		// 		$.proxy( initCount, counter )();
-		// 		$window.on( "scroll", $.proxy( initCount, counter ) );
-		// 	}
-		// }
-
-	// });
-        
        
--- #src/assets/scripts/script.js
+++ #src/assets/scripts/script.js
@@ -1,114 +1,38 @@
 /*_Function  support browser webp css_*/
 @@include('main/_burger.js');
-// @@include('main/_last-works.js');
+
 @@include('main/_scroll-animation.js');
 
 @@include('vendors/owl.carousel.min.js');
 
 
-// const grid = document.querySelector('.grid');
-
-// window.setTimeout(function(){
-
-//     const msnry = new LastWorksGrid( grid, {
-
-//         responsive:{
-//           0:{
-//               columns: 1,
-//               gap: 20
-//           },
-      
-//           576:{
-//               columns: 2,
-//               gap: 20
-//           },
-      
-//           1400:{
-//               columns: 3,
-//               gap: 40
-//           }
-      
-//         }
-//       });
-
-
-// }, 100)
-
-
-// let magicGrid = new MagicGrid({
-//     container: '.last-projects__wrapper',
-//     animate: true,
-//     gutter: 40,
-//     static: true,
-//     useMin: true
-//   });
-
-//   magicGrid.listen();
-
-
 
 let headerInnerHeight = querSell('.header__inner').offsetHeight;
+let contactFormInput = document.querySelector('.subscribe__input');
+let contactFormLable = document.querySelector('.subscribe__label');
 
 
+// resize page
 window.addEventListener("resize", resizePage)
 resizePage()
 
 function resizePage(){
+
    if(window.innerWidth >= 992 && querSell('.sidebar').classList.contains('manu-active')){
         querSell('.sidebar').classList.remove('manu-active');
         querSell('.container-body').classList.remove('contant-active');
     } 
 
-    if(window.innerWidth < 992 ){
-      
-        console.log(headerInnerHeight)
-        querSell('.header').style.height = `${headerInnerHeight}px`;
-    }else if(window.innerWidth > 992){
-        querSell('.header').style.height = `0`; 
-    }
+    if(window.innerWidth < 992 ) querSell('.header').style.height = `${headerInnerHeight}px`;
+    else if(window.innerWidth > 992) querSell('.header').style.height = `0`; 
+    
 
 }
 
 
 
 
-
-$(document).ready(function(){
-
-    // $('.counter__number').counterUp({
-
-    //     delay: 10,
-    //     time: 1200
-    // })
-
-    $(".partners-carosel.owl-carousel").owlCarousel({
-            items: 1,
-        }
-       
-    );
-
-
-     
-    
-    // $(window).scroll(function(){
-       
-    
-    // })
-
-   
-    // setTimeout(function(){
-    //     window.scrollTo(0, 0);
-    // }, 1);
-});
-
-window.onload = function(){
-    // document.body.scrollTop = 700;
-}
-
-
-let contactFormInput = document.querySelector('.subscribe__input');
-let contactFormLable = document.querySelector('.subscribe__label');
-
+// input focus 
 contactFormInput.onfocus =  function(){
     contactFormLable.style.opacity = "0";
 }
@@ -121,3 +45,42 @@ contactFormInput.onblur =  function(){
     } 
    
 }
+
+
+
+
+$(document).ready(function(){
+
+    // owl-carousel 
+    $(".partners-carosel.owl-carousel").owlCarousel({
+            items: 1,
+        }
+       
+    );
+
+    // Counter number 
+    $('.counter__number').counterUp({
+        delay: 10,
+        time: 1200
+    })
+
+ 
+
+
+    //  button scroll up
+    let btn = $('#button');
+
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+    });
+
+    btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '400');
+    });
+    
+});
